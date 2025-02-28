e4711af7af621cd637b54da95636ce63c79b7084
Rahaf Aljerwi <aljerwi.rahaf@yahoo.com>
2025-02-28 14:10:21 -0600

    Initial Commit

---
+++ GitHubCollaborationTests/GitHubCollaborationTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationTests.swift
+//  GitHubCollaborationTests
+//
+//  Created by Rahaf Aljerwi on 2/28/25.
+//
+
+import Testing
+@testable import GitHubCollaboration
+
+struct GitHubCollaborationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
